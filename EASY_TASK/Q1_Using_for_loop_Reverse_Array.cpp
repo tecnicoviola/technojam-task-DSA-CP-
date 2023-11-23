30389dc2136a8e4bfba45e3df596c4ad7426fb91
VIOLA <146161086+tecnicoviola@users.noreply.github.com>
2023-11-23 10:21:29 +0530

    Q1_Using_for_loop_Reverse_Array.cpp

--- EASY_TASK/Q1_Using_for_loop_Reverse_Array.cpp
+++ EASY_TASK/Q1_Using_for_loop_Reverse_Array.cpp
@@ -1,24 +1,24 @@
 #include <iostream>
 using namespace std;
 
-void reverse(int arr[], int n) {
-    int start = 0;
+void reverse(int arr[], int n) {  //function made named reverse this is void so it has no return type
+    int start = 0;   
     int end = n - 1;
     }
 
-int main() {
+int main() {  //driver code
     int arr[] = {1, 2, 3, 4, 5};
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int n = sizeof(arr) / sizeof(arr[0]); //sizeof(arr) tells the size of the array  
     cout << "Previous array: ";
     
     for (int i = 0; i < n; i++) {
         cout << arr[i] << " ";
     }
 
-    reverse(arr, n);
+    reverse(arr, n);  //reverse function is called here
 
     cout << "Reversed array: ";
-    for (int i = n-1; i >=0; i--) {
+    for (int i = n-1; i >=0; i--) { //prints reverse array
         cout << arr[i] << " ";
     }
 
