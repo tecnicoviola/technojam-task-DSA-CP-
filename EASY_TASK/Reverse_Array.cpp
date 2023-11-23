72484a809a31298f83121b50be094db52026e7e3
VIOLA <146161086+tecnicoviola@users.noreply.github.com>
2023-11-23 10:03:59 +0530

    EASY LEVEL QUESTION
    Method 1 another method to solve it is using for loop

---
+++ EASY_TASK/Reverse_Array.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+void reverse(int arr[], int n) {
+    int start = 0;
+    int end = n - 1;
+
+    while (start < end) {
+    
+        int temp = arr[start]; 
+        arr[start] = arr[end];
+        arr[end] = temp;
+
+        start++;
+        end--;
+    }
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << "Previous array: ";
+    
+    for (int i = 0; i < n; ++i) {
+        cout << arr[i] << " ";
+    }
+
+    reverse(arr, n);
+
+    cout << "Reversed array: ";
+    for (int i = 0; i < n; ++i) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
