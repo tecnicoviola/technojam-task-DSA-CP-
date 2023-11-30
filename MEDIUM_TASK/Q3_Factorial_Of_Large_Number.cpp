3d6387721ce53a34971a83bcbecba45d9aeef0ed
VIOLA <146161086+tecnicoviola@users.noreply.github.com>
2023-12-01 00:53:35 +0530

    Update Q3_Factorial_Of_Large_Number.cpp

--- MEDIUM_TASK/Q3_Factorial_Of_Large_Number.cpp
+++ MEDIUM_TASK/Q3_Factorial_Of_Large_Number.cpp
@@ -1,7 +1,23 @@
 #include<iostream>
 using namespace std;
 
-void fact(int n){
-       
- 
+int factorial(int n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    } else {
+        int fact = 1;
+        for (int i = 2; i <= n; ++i) {
+            fact = fact* i;
+        }
+        return fact;
+    }
+}
+
+int main() {
+    int num = 100; 
+    int result = factorial(num);
+    
+    cout << "Factorial is " << result << endl;
+
+    return 0;
 }
